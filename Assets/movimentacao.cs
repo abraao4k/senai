8f08ae18d510f2a341bdd5e3e2202d201a629558
abraao4k <abraaocontato9@gmail.com>
2026-03-03 10:48:12 -0300

    fisica

--- Assets/movimentacao.cs
+++ Assets/movimentacao.cs
@@ -1,21 +1,39 @@
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class movimentacao : MonoBehaviour
 {
+    float vida = 100;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-
+    { 
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (input.GetKey("d")) ;
-        transform.Translate(1, 0, 0);
-        transform.Translate(0, 1, 0);
-        Physics.CheckSphere(transform.position,if);
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(0.03f, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-0.03f, 0, 0);
+        }
+        if (Input.GetKey("w")) { 
+                transform.Translate(0, 0.05f, 0); 
+                    
+         }
+    }
+    //Destroy(this.gameobject);
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "bomba")
+        {
+            vida = vida - 100f;
+            Debug.Log("morreu");
+        }
     }
 }
